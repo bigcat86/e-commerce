5c91ba1b4a26ef7ea910a298309fe605a5538264
Aaron <attanner86@gmail.com>
2023-05-18 18:09:22 -0600

    next route

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,28 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    product_id: {
+       type: DataTypes.STRING,
+       references: Product.id
+    },
+    tag_id: {
+        type: DataTypes.INTEGER,
+        references: Tag.id
+    }
   },
   {
     sequelize,
